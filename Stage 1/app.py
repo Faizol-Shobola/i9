155060882b808ac1fda7c66715600854816ae2e0
Shobola Faizol <shobolafaizol@gmail.com>
2022-10-29 09:21:01 +0100

    Jsonify the response

--- Stage 1/app.py
+++ Stage 1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,10 +10,10 @@ def home():
         "slackUsername": 'Just_Kayfz',
         "backend": True,
         "age": 20,
-        "bio": 'I am a Backend Developer'
+        "bio": 'I am a Full-Stack developer with experience in both frontend and backend technology.'
     }
 
-    return details
+    return jsonify(details)
 
 
 if __name__ == '__main__':
